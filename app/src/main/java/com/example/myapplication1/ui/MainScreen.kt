cd37b161ef8b795d0451354d739b7d0f62ad5c33
Nikolay-ux <nikolael.pochta@gmail.com>
2025-10-22 05:57:10 +0700

    Edit padding

--- app/src/main/java/com/example/myapplication1/ui/MainScreen.kt
+++ app/src/main/java/com/example/myapplication1/ui/MainScreen.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication1.ui
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -14,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.myapplication1.ui.tabs.Authors
 import com.example.myapplication1.ui.tabs.RegForm
 import com.example.myapplication1.ui.tabs.Rules
@@ -33,6 +36,7 @@ fun MainScreen() {
         ) {
             Tab(
                 text = { Text("Игра") },
+                modifier = Modifier.padding(0.dp, 30.dp),
                 selected = selectedTab == 0,
                 onClick = { selectedTab = 0 }
             )
